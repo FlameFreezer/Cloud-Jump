a6cf67cdcfdf132ab39b27863a871bda0955662a
FFreeze <quincymon@icloud.com>
2026-05-15 18:46:42 -0700

    Simple walking and jumping

--- src/Sprites/player.js
+++ src/Sprites/player.js
@@ -1,3 +1,4 @@
+const SMALL_DELTA = 0.01;
 class Player extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, "characters", "tile_0004.png");
@@ -15,23 +16,50 @@ class Player extends Phaser.GameObjects.Sprite {
                 {key: "characters", frame: "tile_0004.png"} 
             ]
         });
+        this.ACCELERATION = 400;
         this.dKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
         this.aKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-
-        this.dKey.on("down", (event) => {
-            this.play("walk");
-        });
-        this.dKey.on("up", (event) => {
-            this.stopAfterRepeat(0);
-        })
-        this.aKey.on("down", (event) => {
-            this.play("walk");
-        });
-        this.aKey.on("up", (event) => {
-            this.stopAfterRepeat(0);
-        });
+        this.spaceKey = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.body.setMaxVelocityX(140);
     }
 
     update(delta) {
+        // Horizontal input polling
+        var input = 0;
+        if(this.dKey.isDown) {
+            input += 1;
+        }
+        if(this.aKey.isDown) {
+            input -= 1;
+        }
+        // Apply horizontal acceleration
+        if(input == 0) {
+            this.body.setAccelerationX(0);
+            this.body.setDragX(this.ACCELERATION);
+        }
+        else {
+            this.body.setAccelerationX(input * this.ACCELERATION);
+        }
+        
+        // Check for jumping
+        if(Phaser.Input.Keyboard.JustDown(this.spaceKey) && this.body.blocked.down) {
+            this.body.velocity.y -= 400;
+        }
+
+        // Flip player sprite based on velocity
+        if(this.body.velocity.x - SMALL_DELTA > 0) {
+            this.flipX = true;
+        }
+        else if(this.body.velocity.x + SMALL_DELTA < 0) {
+            this.flipX = false;
+        }
+
+        // Handle walk animation
+        if(this.body.velocity.x != 0 && !this.anims.isPlaying) {
+            this.play("walk");
+        }
+        else {
+            this.stopAfterRepeat(0);
+        }
     }
 }
